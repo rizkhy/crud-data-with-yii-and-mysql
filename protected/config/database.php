7ea3a065e94bbd52464ef2b96db7474c51e1ee0d
rizkhy <rizky1842@gmail.com>
2019-03-15 02:34:26 +0700

    push heroku

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,10 +5,10 @@ return array(
 	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/K24.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=localhost;dbname=K24',
+	'connectionString' => 'mysql:host=den1.mysql4.gear.host;dbname=k24',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'k24',
+	'password' => 'Ut5_-68WID8y',
 	'charset' => 'utf8',
 	
 );
